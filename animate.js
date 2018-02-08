b94d6906170b9b4987e9d2d06cfd105a205c5afa
BermetKalmakova <bermetkalmakova@gmail.com>
2018-02-07 22:50:08 -0500

    im sleepy

--- animate.js
+++ animate.js
@@ -3,20 +3,48 @@ var ctx = canvas.getContext("2d");
 var start = document.getElementById("start");
 var stop = document.getElementById("stop");
 
-var r = 2;
+var clear = function(e){
+    ctx.clearRect(0,0,500,500);
+}
 
+
+var r = 2;
+var setting = 0;
 var draw = function(e){
-    if(r<240){
+    clear();
+    if (setting == 0){
+	if (r > 230){
+	    setting = 1;
+	}
 	r++;
     }
     else{
-	r--;
+	if (r < 2){
+	    setting = 0;
+	}
+	r --;
     }
     ctx.beginPath();
     ctx.arc(250,250,r,0,2*Math.PI);
     ctx.stroke();
     ctx.fillStyle = "black";
-    ctx.fill()
+    ctx.fill();
+    requestID = window.requestAnimationFrame(draw);
 }
 
+var end = function(e){
+    window.cancelAnimationFrame(requestID);
+}
+
+var reset = function(e){
+    window.cancelAnimationFrame(requestID);
+    r = 2;
+    setting = 0;
+    draw();
+}
+
+stop.addEventListener("click",end);
+
 start.addEventListener("click",draw);
+
+canvas.addEventListener("click",reset);
